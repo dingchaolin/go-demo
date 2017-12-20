5f81d31214b981e0f2110f91cc48061961db9a8b
dingchaolin <18810042351@163.com>
2017-12-20 21:00:15 +0800

    feat: if for goto break continue

---
+++ src/005_break.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+
+
+ func main(){
+
+	 LABEL1:
+	 	for{
+	 		for i := 0; i < 5; i++{
+	 			if i > 3 {
+	 				break LABEL1 //会调到LABEL1 同级处 执行 fmt.Println( "OK" )代码
+	 				// goto LABEL1 //将程序调整到LABEL1处  尽量将将标签放到goto之后
+	 				//continue LABEL1  死循环
+				}
+			}
+		}
+
+		fmt.Println( "OK" )
+
+
+
+ }
