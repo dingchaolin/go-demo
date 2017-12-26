e340255f5e18142800096468f13fac9acfed0fc8
dingchaolin <18810042351@163.com>
2017-12-26 17:00:31 +0800

    feat:slice params

---
+++ src/015_env.go
@@ -0,0 +1,11 @@
+package main
+import (
+	"fmt"
+	"os"  //我们要用到os包中的env
+)
+func main() {
+	//os.Getenv检索环境变量并返回值，如果变量是不存在的，这将是空的。
+	HOME:= os.Getenv("HOME")
+	fmt.Println(HOME)
+	fmt.Printf(os.Getenv("JAVA_HOME"))
+}
