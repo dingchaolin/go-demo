bec19a7efe0e8f432019322f5b3483ccee3df10d
dingchaolin <18810042351@163.com>
2018-02-25 23:41:45 +0800

    feat: jiaoji

--- src/strArrJiaoJi/jiaoji/jiaoji.go
+++ src/strArrJiaoJi/jiaoji/jiaoji.go
@@ -1,5 +1,7 @@
 package intersection
 
+//import "errors"
+
 func intersection(strArr1 []string, strArr2 []string) []string {
 	var jiaojiArr []string
 
@@ -35,3 +37,34 @@ func Intersection(strArr1, strArr2 []string, args ...interface{}) []string {
 	}
 }
 
+
+
+//func Intersection(args ...interface{}) ([]string, error) {
+//	var strArr1, strArr2 []string
+//	if len( args ) < 2 {
+//		return nil, errors.New("args length must >= 2")
+//	} else {
+//		value0, ok0 := args[0].([]string)
+//		value1, ok1 := args[1].([]string)
+//		if len(args) == 2 {
+//			if  ok0 && ok1 {
+//				arr := intersection(value0, value1)
+//				return arr, nil
+//			}else{
+//				return nil, errors.New("args item must be string array")
+//			}
+//		} else {
+//			jiaojiArr := intersection(strArr1, strArr2)
+//			for _, item := range args {
+//				if value, ok := item.([]string); ok {
+//					jiaojiArr = intersection(jiaojiArr, value)
+//					if len(jiaojiArr) == 0 {
+//						return jiaojiArr
+//					}
+//				}
+//
+//			}
+//			return jiaojiArr
+//		}
+//	}
+//}
